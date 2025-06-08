166f31ba2caef215084e004a42de400f7ae58843
DianaEoeoeoe <dianadudnyk2013@gmail.com>
2025-06-08 14:19:14 +0200

    lesson 1

---
+++ hw3.py
@@ -0,0 +1,82 @@
+import random
+
+
+class Cat:
+    def __init__(self, name="Cat", home=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.home = home
+
+    def get_home(self):
+        self.home = House()
+
+    def eat(self):
+        if self.home.food <= 0:
+            self.shopping("food")
+        else:
+            if self.satiety >= 100:
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
+    def clean_home(self):
+        self.gladness -= 5
+        self.home.mess = 0
+    def to_repair(self):
+        self.car.strength += 100
+        self.money -= 50
+
+    def days_indexes(self, day):
+        day = f" Today the {day} of {self.name}'s life "
+        print(f"{day:=^50}", "\n")
+        human_indexes = self.name + "'s indexes"
+        print(f"{human_indexes:^50}", "\n")
+        print(f"Money – {self.money}")
+        print(f"Satiety – {self.satiety}")
+        print(f"Gladness – {self.gladness}")
+
+    def is_alive(self):
+        if self.gladness < 0:
+            print("Depression…")
+            return False
+        if self.satiety < 0:
+            print("Dead…")
+            return False
+        if self.money < -500:
+            print("Bankrupt…")
+            return False
+
+    def live(self, day):
+        if self.is_alive() == False:
+            return False
+        if self.home is None:
+            print("Settled in the house")
+            self.get_home()
+        self.days_indexes(day)
+        dice = random.randint(1, 4)
+        if self.satiety < 20:
+            print("I'll go eat")
+            self.eat()
+        elif self.gladness < 20:
+                print("Let`s chill!")
+                self.chill()
+        elif dice == 1:
+            print("Let`s chill!")
+            self.chill()
+
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+Murka = Cat(name="Murka")
+for day in range(1, 800):
+    if Murka.live(day) == False:
+        break
